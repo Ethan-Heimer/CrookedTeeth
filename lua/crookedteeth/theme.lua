70f1c48e13076d986d985cdcdf442e6952797b1c
Ethan-Heimer <ethan.heimer12357@gmail.com>
2025-07-23 00:00:27 -0700

    Theme Rev 1

--- lua/crookedteeth/theme.lua
+++ lua/crookedteeth/theme.lua
@@ -1,73 +1,71 @@
 local crooked = require("crookedtheme")
 
-local backgroundOne = "none"
-local backagroundTwo = "none"
-
-local colorOne = "#FFF6F9"
-local colorTwo = "#FFC8DD"
-local colorThree = "#FFBCD5"
-local colorFour = "#FF99BE"
-local colorFive = "#FCF5DE"
-local colorSix = "#FEF2CD"
-local colorSeven = "#FFEFBC"
-local colorEight = "#BDE0FE"
-local colorNine = "#B0D9FF"
-local colorTen = "#A2D2FF"
+local colors = {
+    Clear = "none",
+    white = "#ffffff",
+    Gray = "#B5A0B5",
+    Black = "#1A1423",
+    DarkPurple = "#372549",
+    Purple = "#774C60",
+    Red = "#B75D69",
+    Orange = "#F3A87C",
+    Pink = "#F6A2C9",
+}
 
 local theme = crooked.NewTheme();
 
-theme.name = "Pastel"
+theme.name = "Crooked Teeth"
 
-theme.base.number = {fg = colorSeven, underline = true}
-theme.base.string = {fg = colorTwo, italic = true, underline = true}
-theme.base.type = {fg = colorSeven, bold = true}
-theme.base.method = {fg = colorThree, bold = true}
-theme.base.identifier = {fg = colorTwo}
-theme.base.statement = {fg = colorFive}
-theme.base.comment = {fg = colorFour, italic = true}
-theme.base.constant = {fg = colorSeven, underline = true}
-theme.base.preproc = {fg = colorTwo, bold = true}
-theme.base.symbol = {fg = colorEight}
-theme.base.boolean = {fg = colorFour, bold = true, underline = true}
-theme.base.keyword = {fg = colorSeven}
+theme.base.number = {fg = colors.Pink, underline = true}
+theme.base.string = {fg = colors.Pink, italic = true, underline = true}
+theme.base.type = {fg = colors.Red, bold = true}
+theme.base.method = {fg = colors.Orange, bold = true}
+theme.base.identifier = {fg = colors.DarkPurple}
+theme.base.statement = {fg = colors.Purple}
+theme.base.comment = {fg = colors.Gray, italic = true}
+theme.base.constant = {fg = colors.Pink, underline = true}
+theme.base.preproc = {fg = colors.Orange, bold = true}
+theme.base.symbol = {fg = colors.Orange}
+theme.base.boolean = {fg = colors.Pink, bold = true, underline = true}
+theme.base.keyword = {fg = colors.DarkPurple}
 
-theme.editor.lineNumber = {fg = colorFive, bg = backagroundTwo, bold = true}
-theme.editor.colorColumn = {bg = backagroundTwo}
-theme.editor.background = {bg = backgroundOne}
-theme.editor.cursorColumn = {bg = backagroundTwo}
-theme.editor.indentLine = {fg = colorTen}
-theme.editor.indentScope = {fg = colorThree}
-theme.editor.error = {bg = colorFour, fg = "#000000"}
-theme.editor.warning = {fg = colorFive}
-theme.editor.menu = {bg = backagroundTwo}
-theme.editor.title = {fg = colorFive}
-theme.editor.search = {fg = colorFive}
-theme.editor.nontext = {fg = colorFour}
+theme.editor.lineNumber = {fg = colors.Gray, bg = colors.Black, bold = true}
+theme.editor.colorColumn = {bg = colors.Black}
+theme.editor.background = {bg = colors.Clear}
+theme.editor.cursorColumn = {bg = colors.Black}
+theme.editor.indentLine = {fg = colors.DarkBlack}
+theme.editor.indentScope = {fg = colors.Red}
+theme.editor.error = {bg = colors.Red, fg = "#000000"}
+theme.editor.warning = {fg = colors.Orange}
+theme.editor.menu = {bg = colors.Clear}
+theme.editor.title = {fg = colors.Gray}
+theme.editor.search = {fg = colors.Gray}
+theme.editor.nontext = {fg = colors.Gray}
 
-theme.netrw.dir = {fg = colorSeven}
-theme.netrw.exe = {fg = colorFive}
-theme.netrw.symlink = {fg = colorTen}
-theme.netrw.version = {fg = colorFour}
-theme.netrw.gray = {fg = colorOne}
-theme.netrw.date = {fg = colorTen}
-theme.netrw.time = {fg = colorSeven}
-theme.netrw.comment = {fg = colorFour}
-theme.netrw.base = {fg = colorSeven}
-theme.netrw.change = {fg = colorThree}
-theme.netrw.cursorLine = {bg = backagroundTwo}
-theme.netrw.folder = {fg = colorSeven}
-theme.netrw.image = {fg = colorSix}
+theme.netrw.dir = {fg = colors.Orange}
+theme.netrw.exe = {fg = colors.Pink}
+theme.netrw.symlink = {fg = colors.Orange}
+theme.netrw.version = {fg = colors.DarkPurple}
+theme.netrw.gray = {fg = colors.Gray}
+theme.netrw.date = {fg = colors.Purple}
+theme.netrw.time = {fg = colors.Purple}
+theme.netrw.comment = {fg = colors.Gray}
+theme.netrw.base = {fg = colors.Red}
+theme.netrw.change = {fg = colors.Orange}
+theme.netrw.cursorLine = {bg = colors.Black}
+theme.netrw.folder = {fg = colors.Orange}
+theme.netrw.image = {fg = colors.Red}
 
-theme.keyword.modifier = {fg = colorEight}
-theme.keyword.builtinType = {fg = colorSeven, bold = true, italic = true}
-theme.keyword.builtinMethod = {fg = colorSeven, bold = true, italic = true}
-theme.keyword.loop = {fg = colorSeven, italic = true}
-theme.keyword.returning = {fg = colorFive, underline = true, italic = true, bold = true}
-theme.keyword.conditional = {fg = colorSix, bold = true}
-theme.keyword.macroConstant = {fg = colorSeven, underline = true}
-theme.keyword.import = {fg = colorSeven, italic = true}
-theme.keyword.define = {fg  = colorThree, italic = true}
-theme.keyword.builtinConstant = {fg = colorSeven, underline = true, bold = true}
-theme.keyword.definitions = {fg = colorThree, bold = true, italic = true}
+theme.keyword.modifier = {fg = colors.Purple}
+theme.keyword.builtinType = {fg = colors.Pink, bold = true, italic = true}
+theme.keyword.builtinMethod = {fg = colors.Pink, bold = true, italic = true}
+theme.keyword.loop = {fg = colors.Pink, italic = true}
+theme.keyword.returning = {fg = colors.Pink, underline = true, italic = true, bold = true}
+theme.keyword.conditional = {fg = colors.Red, bold = true}
+theme.keyword.macroConstant = {fg = colors.Red, underline = true}
+theme.keyword.import = {fg = colors.Purple, italic = true}
+theme.keyword.define = {fg  = colors.Red, italic = true}
+theme.keyword.builtinConstant = {fg = colors.Pink, underline = true, bold = true}
+theme.keyword.definitions = {fg = colors.Red, bold = true, italic = true}
 
 return theme;

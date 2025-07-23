d6dbdf02d7afda9e129664c20d19ede4f98ab5d8
Ethan-Heimer <ethan.heimer12357@gmail.com>
2025-07-23 00:25:11 -0700

    Theme Dev

--- lua/crookedteeth/theme.lua
+++ lua/crookedteeth/theme.lua
@@ -23,7 +23,7 @@ theme.base.number = {fg = colors.Pink, underline = true}
 theme.base.string = {fg = colors.Pink, italic = true, underline = true}
 theme.base.type = {fg = colors.Red, bold = true}
 theme.base.method = {fg = colors.Orange, bold = true}
-theme.base.identifier = {fg = colors.Blue}
+theme.base.identifier = {fg = colors.LighBlue}
 theme.base.statement = {fg = colors.Purple}
 theme.base.comment = {fg = colors.LightBlue, italic = true}
 theme.base.constant = {fg = colors.Pink, underline = true}
@@ -70,5 +70,6 @@ theme.keyword.import = {fg = colors.Purple, italic = true}
 theme.keyword.define = {fg  = colors.Red, italic = true}
 theme.keyword.builtinConstant = {fg = colors.Pink, underline = true, bold = true}
 theme.keyword.definitions = {fg = colors.Red, bold = true, italic = true}
+theme.keyword.variable = {fg = colors.Blue};
 
 return theme;
